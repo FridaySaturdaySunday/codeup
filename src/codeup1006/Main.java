d8a1919573bf7cde352628503734c5ae07c404b9
cks <cksun2598@gmail.com>
2022-09-15 21:09:20 +0900

    d

--- src/codeup1006/Main.java
+++ src/codeup1006/Main.java
@@ -3,8 +3,12 @@ package codeup1006;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.printf("\"!@#$%%^&*()\"");
+		
+//		printf 에서
+//		% : 형식 지정자(format specifier), 위치에서 지정한 형식으로 값을 출력해 준다.
+//		따라서, 퍼센트 문자 %를 출력하기 위해서는 %%로 출력해 주어야 한다.
 	}
 
 }
---
+++ src/codeup1007/Main.java
@@ -0,0 +1,12 @@
+package codeup1007;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("\"C:\\Download\\hello.cpp\"");
+		
+//		백슬래시 문자 \ 를 출력하기 위해서는 \\ 로 출력해야 한다.
+	}
+
+}
